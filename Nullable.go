7c2cc8c300a95c8896f644a61a0adc4bb1c11082
guoapeng <guoapeng@gmail.com>
2019-08-25 20:30:40 +0800

    update data type name

--- Nullable.go
+++ Nullable.go
@@ -5,14 +5,50 @@ import (
 	"encoding/json"
 )
 
-type NullableString struct {
+type String struct {
 	sql.NullString
 }
 
-func (value *NullableString) MarshalJSON() ([]byte, error) {
+func (value *String) MarshalJSON() ([]byte, error) {
 	if !value.Valid {
 		return json.Marshal(nil)
 	} else {
 		return json.Marshal(value.String)
+	}
+}
+
+type Int64 struct {
+	sql.NullInt64
+}
+
+func (value *Int64) MarshalJSON() ([]byte, error) {
+	if !value.Valid {
+		return json.Marshal(nil)
+	} else {
+		return json.Marshal(value.Int64)
+	}
+}
+
+type Float64 struct {
+	sql.NullFloat64
+}
+
+func (value *Float64) MarshalJSON() ([]byte, error) {
+	if !value.Valid {
+		return json.Marshal(nil)
+	} else {
+		return json.Marshal(value.Float64)
+	}
+}
+
+type Bool struct {
+	sql.NullBool
+}
+
+func (value *Bool) MarshalJSON() ([]byte, error) {
+	if !value.Valid {
+		return json.Marshal(nil)
+	} else {
+		return json.Marshal(value.Bool)
 	}
 }
--- Nullable_test.go
+++ Nullable_test.go
@@ -5,18 +5,31 @@ import (
 	"testing"
 )
 
-func TestNullableString(t *testing.T){
-
+func TestNullableString(t *testing.T) {
 	for _, unit := range []struct {
-		m        *NullableString
-		n        int
+		value    *String
 		expected string
 	}{
-	{&NullableString{sql.NullString{"aquas", true}}, 0,  "\"aquas\""},
-		{&NullableString{sql.NullString{"aquas", false}}, 0,  "null"},
-		{&NullableString{sql.NullString{"", false}}, 0,  "null"},
+		{&String{sql.NullString{"aquas", true}}, "\"aquas\""},
+		{&String{sql.NullString{"aquas", false}}, "null"},
+		{&String{sql.NullString{"", false}}, "null"},
 	} {
-		if actually, _ := unit.m.MarshalJSON(); string(actually) != unit.expected {
+		if actually, _ := unit.value.MarshalJSON(); string(actually) != unit.expected {
+			t.Errorf("combination: [%v], type:%T, actually: [%v], type: %T", unit.expected, unit.expected, string(actually), string(actually))
+		}
+	}
+}
+
+func TestNullableInt64(t *testing.T) {
+	for _, unit := range []struct {
+		value    *Int64
+		expected string
+	}{
+		{&Int64{sql.NullInt64{0, false}}, "null"},
+		{&Int64{sql.NullInt64{10, false}}, "null"},
+		{&Int64{sql.NullInt64{10, true}}, "10"},
+	} {
+		if actually, _ := unit.value.MarshalJSON(); string(actually) != unit.expected {
 			t.Errorf("combination: [%v], type:%T, actually: [%v], type: %T", unit.expected, unit.expected, string(actually), string(actually))
 		}
 	}
